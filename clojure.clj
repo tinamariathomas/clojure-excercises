38083ab430bb8bb1eafe365b2db1756ea9d6dc2d
tinamt <codertina@gmail.com>
2017-02-28 22:15:36 +0530

    Tina | Method to double every other digit
    [Integer] -> [Integer]

--- clojure.clj
+++ clojure.clj
@@ -2,3 +2,7 @@
                (cond 
                	(< inputNumber 1) []
                	:else (concat (toDigits (quot inputNumber 10)) [(rem inputNumber 10)]))))
+
+(def doubleEveryOther(fn [digitList]
+		(reverse (map * (reverse digitList) (cycle [1 2])))
+	))
