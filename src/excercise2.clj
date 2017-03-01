d4e04ae10e3e44f95131e7d01355d66e59690b94
tinamt <codertina@gmail.com>
2017-03-01 23:07:38 +0530

    Tina | Skip method
    inputString = h e l l o !
    
    output = ["hello!", "el!", "l!", "l", "o", "!"]

--- src/excercise2.clj
+++ src/excercise2.clj
@@ -1,3 +1,8 @@
 (def getAllNths(fn [inputString, n]
 		(map #(nth inputString %1)  (range (- n 1) (count inputString) n) )		
 	))
+
+
+( def skip(fn [inputString]
+	(map #(getAllNths inputString %1)(range 1 (count inputString)))
+	))
