7a6361c8584c953b973de298ef3d1e17983ae637
tinamt <codertina@gmail.com>
2017-03-10 08:49:27 +0530

    Tina | Compute histogram for a list of numbers

--- src/homework3.clj
+++ src/homework3.clj
@@ -4,3 +4,9 @@
 		(+ (getCount (subvec numbers 0 1) searchNumber)(getCount (subvec numbers 1) searchNumber))
 		)
 ))
+
+(defstruct histogramStruct :number :count)
+
+(def histogram( fn [numbers]
+	(map #(struct histogramStruct %1 (getCount numbers %1))(range 1 9))
+	))
